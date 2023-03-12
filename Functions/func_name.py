f6c89faffa3a40b3756b3420c42f3ecd625af5b7
arizeworks <arizeworks@gmail.com>
2023-03-13 00:06:48 +0900

    Update

--- Functions/func_name.py
+++ Functions/func_name.py
@@ -3,9 +3,14 @@ import re
 def title_except_uppercase(s):
     words = s.split(' ')
     for i, word in enumerate(words):
-        if not word.isupper():
+        if word in ["to", "of", "at", "in", "from", "not"]:
+            pass
+        elif len(word) == 2:
+            words[i] = word.upper()
+        elif not word.isupper():
             words[i] = word.capitalize()
     return ' '.join(words)
+
 
 def camel_to_snake(text):
     text = re.sub("((?<=[a-z0-9])[A-Z]|(?!^)[A-Z](?=[a-z]))", r" \1", text)
